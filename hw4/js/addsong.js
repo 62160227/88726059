82a8d845cd4427ca5c106a6c397e89d5cbe3339c
62160227 <62160227@go.buu.ac.th>
2021-01-13 13:19:41 +0700

    hw4

---
+++ hw4/js/addsong.js
@@ -0,0 +1,27 @@
+function addsong(){
+        songs = localStorage.getItem('songs');
+        if(songs === null){
+            songs = [];
+            
+        }else{
+            songs = JSON.parse(songs);
+        }
+        data = [
+            namesong = document.getElementById('addsong').value,
+            artist = document.getElementById('artist').value,
+            lyrics = document.getElementById('lyrics').value
+        ];
+            songs.push(data);
+        
+        localStorage.setItem('songs',JSON.stringify(songs));
+        document.getElementById('addsong').value =''
+        document.getElementById('artist').value =''
+        document.getElementById('lyrics').value =''
+
+    }
+
+function Clearform(){
+    document.getElementById('addsong').value =''
+    document.getElementById('artist').value =''
+    document.getElementById('lyrics').value =''
+}
